9e0fa51a29aa2ac37bca85990e714d3c44b19737
Andrei Tognolo <andreitognolo@gmail.com>
2016-04-01 09:36:48 -0300

    shell:bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -829,7 +829,7 @@ var _              = require('lodash'),
         // `bower` does have some quirks, such as not running as root. If you have problems please try running
         // `grunt init --verbose` to see if there are any errors.
         grunt.registerTask('init', 'Prepare the project for development',
-            ['shell:ember:init']);
+            ['shell:ember:init', 'shell:bower']);
 
         grunt.registerTask('hello', 'Log some stuff.', function() {
             grunt.log.write('Hello, hello, is there anybody in there?').ok();
